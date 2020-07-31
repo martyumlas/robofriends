dd16228405ac524abc22e1a82bb868e5cb5c32af
Marty Umlas <martyumlas@gmail.com>
2020-07-31 22:56:48 +0800

    Updates

---
+++ precache-manifest.ae7f01eb0571da211eeabb1d7b0c4d6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af55d20d8dd89af8ce0869f753dc28d3",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "8bfd9a46423adc120756",
+    "url": "/robofriends/static/js/2.65eac47e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.65eac47e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3071ff754f30c83b2cff",
+    "url": "/robofriends/static/js/main.048eee37.chunk.js"
+  },
+  {
+    "revision": "ac9cd7f588cfa4e6395e",
+    "url": "/robofriends/static/js/runtime-main.6b8a5f5c.js"
+  }
+]);
